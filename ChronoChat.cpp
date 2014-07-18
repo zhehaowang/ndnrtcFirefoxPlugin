dea1595922819b84311f18bdb8d531ef376284d1
Zhehao Wang <wangzhehao410305@gmail.com>
2014-07-18 16:52:28 -0700

    Started trying ChronoChat-direct

---
+++ ChronoChat.cpp
@@ -0,0 +1,9 @@
+//
+//  ChronoChat.cpp
+//  BasicPlugin
+//
+//  Created by ZhehaoWang on 7/18/14.
+//
+//
+
+#include "ChronoChat.h"
---
+++ ChronoChat.h
@@ -0,0 +1,14 @@
+//
+//  ChronoChat.h
+//  BasicPlugin
+//
+//  Created by ZhehaoWang on 7/18/14.
+//
+//
+
+#ifndef __BasicPlugin__ChronoChat__
+#define __BasicPlugin__ChronoChat__
+
+#include <iostream>
+
+#endif /* defined(__BasicPlugin__ChronoChat__) */
